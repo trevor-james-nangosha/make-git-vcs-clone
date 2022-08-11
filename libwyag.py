6c7d747f4962ba9cb5f323e3126d48cd3aee75bc
trevor-james-nangosha <trevornangosha16@gmail.com>
2022-08-11 12:09:46 +0300

    finished the repo creation logic

--- libwyag.py
+++ libwyag.py
@@ -1,7 +1,7 @@
 import argparse
 import collections
 import configparser
-import hashlib 
+import hashlib
 import os
 import re
 import sys
@@ -29,4 +29,95 @@ def main(argv = sys.argv[1:]):
     elif args.command == "show-ref": cmd_show_ref(args)
     elif args.command == "tag": cmd_tag(args)
 
+class GitRepository(object):
+    """A git repository"""
+    work_tree = None
+    git_dir = None
+    conf = None
+
+    def __init__(self, path, force=False):
+        self.work_tree = path
+        self.git_dir = os.path.join(path, '.git')
+
+        if not (force or os.path.isdir(self.git_dir) ):
+            raise Exception(f'not a git repository {path}')
+
+        # read the configuration file in .git.config
+        self.conf = configparser.ConfigParser()
+        cf = repo_file(self, 'config')
+
+        if cf and os.path.exists(cf):
+            self.conf.read([cf])
+        elif not force:
+            raise Exception('configuration file is missing')
+
+        if not force:
+            vers = int(self.conf.get('core', 'repositoryformatversion'))
+            if vers != 0:
+                raise Exception(f"unsupported repository format version {}")
+
+def repo_path(repo, *path):
+    """Compute path under the repo's git_dir"""
+    return os.path.join(repo.git_dir, *path)
+
+def repo_file(repo, *path, mkdir=False):
+    """same as repo_path, but mkdir *path if absent if mkdir"""
+    path = repo_path(repo, *path)
+    
+    if os.path.exists(path):
+        if os.path.isdir(path):
+            return path 
+        else:
+            raise Exception(f"Not a directory: {path}")
+
+        if mkdir:
+            os.makedirs(path)
+            return path 
+        else:
+            return None 
+
+def repo_create(path):
+    """create a new repository at path."""
+
+    repo = GitRepository(path, True)
+
+    # first, we make sure that the path either does not exist or is 
+    # an empty dir
+    if os.path.exists(repo.work_tree):
+        if not os.path.isdir(repo.work_tree):
+            raise Exception(f"{path} is not a directory.")
+        if os.listdir(repo.work_tree):
+            raise Exception(f"{path} is not empty")
+
+    else:
+        os.makedirs(repo.work_tree)
+    
+    assert(repo_dir(repo, "branches", mkdir=True))
+    assert(repo_dir(repo, "objects", mkdir=True))
+    assert(repo_dir(repo, "refs", "tags", mkdir=True))
+    assert(repo_dir(repo, "refs", "heads", mkdir=True))
+
+    # .git/description
+    with open(repo_file(repo, "description"), "w") as f:
+        f.write("Unnamed repository: edit this file description")
+
+
+    # .git/HEAD
+    with open(repo_file(repo, "HEAD"), "w") as f:
+        f.write("ref: refs/heads/master\n")
+
+    with open(repo_file(repo, "config"), "w") as f:
+        config = repo_default_config()
+        config.write(f)
+
+    return repo
+
+def repo_default_config():
+    ret = configparser.ConfigParser()
+    ret.add_section('core')
+    ret.set('core', 'repositoryformatversion', '0')
+    ret.set('core', 'filemode', 'false')
+    ret.set('core', 'bare', 'false')
+
+    return ret
 
