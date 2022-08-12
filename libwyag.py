d5e4563ee220709a68dbe0b12afc094af0e05bfb
trevor-james-nangosha <trevornangosha16@gmail.com>
2022-08-12 13:55:21 +0300

    support for git indexes

--- libwyag.py
+++ libwyag.py
@@ -695,5 +695,38 @@ def cmd_rev_parse(args):
         fmt = args.type.encode()
 
     repo = repo_find()
-    
+
     print (object_find(repo, args.name, fmt, follow=True))
+
+
+########################### git indexes #########################
+class GitIndexEntry(object):
+    ctime = None
+    """The last time a file's metadata changed. This is a tuple (seconds, )"""
+    
+    mtime = None
+    """The last time a file's data changed. This is a tuple (seconds,)"""
+    
+    dev = None
+    """The ID of device containing this file"""
+    ino = None
+    """The file's inode number"""
+    mode_type = None
+    """The object type, either b1000 (regular), b1010 (symlink), b1110"""
+    mode_perms = None
+    """The object permissions, an integer."""
+    uid = None
+    """User ID of owner"""
+    gid = None
+    """Group ID of ownner (according to stat 2. Isn'th)"""
+    size = None
+    """Size of this object, in bytes"""
+    obj = None
+    """The object's hash as a hex string"""
+    flag_assume_valid = None
+    flag_extended = None
+    flag_stage = None
+    flag_name_length = None
+    """Length of the name if < 0xFFF (yes, three Fs), -1 otherwise"""
+    
+    name = None
